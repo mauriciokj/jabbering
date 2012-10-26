219039a0ad1d55126f62514b03453ec49eaf081b
Mauricio <mauriciokj@gmail.com>
2012-10-26 17:13:25 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jabbering::Application.config.secret_token = '78173f12782f4301f93d590cc172796ad0a83ef5191b8bdae734b5a9f5babf50ec2dba361a879c3cd1e8f620eb578e03b379a0cb9eedcdd833fdd4163b737df2'
